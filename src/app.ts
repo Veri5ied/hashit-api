f5f7f44fbe0e6aaf978efcdf94fed54b72131b1d
Veri5ied <alvinuchenna14@gmail.com>
2023-09-06 09:56:08 +0100

    cors error handling

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,13 @@ import "./database/db";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000" || "https://hashit.xyz",
+    credentials: true,
+    optionsSuccessStatus: 200 || 204,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
