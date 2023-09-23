4ec3bad866f05a101b65e0dbb3faa4576f3749e6
Veri5ied <alvinuchenna14@gmail.com>
2023-09-23 07:02:12 +0100

    email field

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,12 +5,14 @@ interface User extends Document {
   username: string;
   password: string;
   messages: MessageDocument[];
+  email?: string;
 }
 
 const userSchema = new Schema<User>({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+  email: { type: String, required: false },
 });
 
 export default mongoose.model<User>("User", userSchema);
